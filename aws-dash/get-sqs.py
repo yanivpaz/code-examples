0c987d90c9e696b37a2c111755947142d183a5bd
Yaniv Paz <yanivpaz1973@gmail.com>
2020-04-18 23:03:42 +0300

    Update get-sqs.py

--- aws-dash/get-sqs.py
+++ aws-dash/get-sqs.py
@@ -15,4 +15,25 @@ while True:
         if message.message_attribute is not None:
             author_name = message.message_attributes.get('Author').get('StringValues')
             if author_name:
-
+                author_text = ' ({0})'.format(author_name)
+        else:
+          print ('message from IOT : {0}'.format (message.body))
+          data = json.loads(message.body)
+          if data["serialNumber"] == "GG"
+             data["username"] = "yaniv"
+          clickType = data["clickType"]
+          if clickType == "SINGLE"
+            print ("Recedived SINGLE");
+            message.delete()
+            data["cmp"] = "XXX"
+          elif clickType == "DOUBLE"
+            print ("Recedived SINGLE");
+            message.delete()
+            data["cmp"] = "YYY"
+            subprocess.call ("run_command",shell=True)
+          elif clickType == "LONG"
+            print ("Recedived SINGLE");
+            message.delete()
+            data["cmp"] = "YYY"
+            subprocess.call ("run_command",shell=True)
+    time.sleep (2)
